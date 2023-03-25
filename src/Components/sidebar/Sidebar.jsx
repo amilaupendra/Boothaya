b30c8d496d4c255b221f1d268e080573dbee4be3
amilaupendra <amilaupendra5@gmail.com>
2023-03-25 23:49:36 +0530

    dashboard changes added

--- src/Components/sidebar/Sidebar.jsx
+++ src/Components/sidebar/Sidebar.jsx
@@ -1,45 +1,42 @@
-import React, {useState} from 'react'
-import './sidebar.css'
-import { FaBars } from 'react-icons/fa';
-import { AiOutlineClose } from 'react-icons/ai';
-import { Link } from 'react-router-dom';
-import { SidebarData } from './Sidebardata';
-import { IconContext } from 'react-icons';
+import React, { useState } from "react";
+import "./sidebar.css";
+import { FaBars } from "react-icons/fa";
+import { GiThreeLeaves } from "react-icons/gi";
+import { Link } from "react-router-dom";
+import { SidebarData } from "./Sidebardata";
+import { IconContext } from "react-icons";
 
 export default function Sidebar() {
-    const [sidebar, setSidebar]= useState(false)
+  const [sidebar, setSidebar] = useState(false);
 
-    const showSidebar = () => setSidebar(!sidebar);
+  const showSidebar = () => setSidebar(!sidebar);
 
   return (
     <div>
-        <IconContext.Provider value={{color:'fff'}}>
+      <IconContext.Provider value={{ color: "fff" }}>
         <div className="sidebar">
-            <Link to="#" className='menu-bars'>
-            <FaBars onClick={showSidebar} />
-            </Link>
-
-            <nav className={sidebar ? 'nav-menu active' : 'nav-menu'}>
-                <ul className='nav-menu-items' onClick={showSidebar}>
-                    <li className='navbar-toggle'>
-                        <Link to='#' className='menu-bars'>
-                            <AiOutlineClose/>
-                        </Link>
-                    </li>
-                    {SidebarData.map((item, index) => {
-                        return(
-                            <li key={index} className={item.cName}>
-                                <Link to={item.path}> 
-                                {item.icon}
-                                <span>{item.title}</span>
-                                    </Link></li>
-                        )
-                    })}
-                </ul>
-            </nav>
+          <nav className={sidebar ? "nav-menu active" : "nav-menu"}>
+            <ul className="nav-menu-items" onClick={showSidebar}>
+              {SidebarData.map((item, index) => {
+                return (
+                  <li key={index} className={item.cName}>
+                    <Link to={item.path}>
+                      {item.icon}
+                      <span>{item.title}</span>
+                    </Link>
+                  </li>
+                );
+              })}
+              <li>
+                {" "}
+                <span>
+                  <img src={GiThreeLeaves} alt="XXX" />
+                </span>
+              </li>
+            </ul>
+          </nav>
         </div>
-        </IconContext.Provider>
-        
+      </IconContext.Provider>
     </div>
-  )
+  );
 }
--- src/pages/farmerControl/FarmerControl.jsx
+++ src/pages/farmerControl/FarmerControl.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
 import './farmerControl.css'
+import Sidebar from '../../Components/sidebar/Sidebar'
+
 export default function FarmerControl() {
   return (
-    <div className='farmer-control-div'>FarmerControl</div>
+    <div className='farmer-control-div'>
+        <Sidebar />
+        hello
+    </div>
   )
 }
